b1f3b4c5e64bd99ba516f6303f3194f443f13500
Sanjit Kumar <sanjitk2018@gmail.com>
2020-07-09 13:12:49 +0530

    dashboard update
    - attendance exists check
    - attendance table

--- dashboard.py
+++ dashboard.py
@@ -2,14 +2,26 @@ import tkinter as tk
 from db import mycursor,mydb
 from datetime import datetime
 
-now = datetime.now()
+now = datetime.now().date()
 formatted_date = now.strftime('%Y-%m-%d %H:%M:%S')
 
 u_id = "1"
 password = "Hello12123"
 
+def checkAttendance():
+    checkFlag=0
+    mycursor.execute("""select * from attendance_record where a_date=%s and u_id=%s""",(formatted_date,u_id))
+    dbAttdCheck = mycursor.fetchall()
+    if (dbAttdCheck!=[]):
+        print("Attendance Already Posted")
+        checkFlag=1
+    return checkFlag
+
 def postAttendance(event):
-    mycursor.execute("""insert into attendance_record(a_date,a_detail,u_id) value (%s,%s,%s)""",(formatted_date,"PRESENT",1))
+    checkFlag = checkAttendance()
+    if(checkFlag==1):
+        return
+    mycursor.execute("""insert into attendance_record(a_date,a_detail,u_id) value (%s,%s,%s)""",(formatted_date,"PRESENT",u_id))
     mydb.commit()
     print("ATTENDANCE POSTED!")
 
@@ -23,33 +35,42 @@ attdFrame = tk.Frame(root)
 titleLabel = tk.Label(titleFrame,text="PROFILE AND STATS", fg="red",font=('Arial',20,'bold'))
 titleLabel.grid(sticky=tk.N)
 
-titleFrame.grid(sticky=tk.N)
+titleFrame.grid(row=0,sticky=tk.N)
 
 # get user details
 mycursor.execute("select * from t_users where u_id=%s",(u_id,))
 dbDetails = mycursor.fetchall()
 headers=("User Id","Password","Date of Joining","Full Name")
 dbDetails.insert(0,headers)
-print(dbDetails)
+print("DB DETAILS OF CURRENT USER FETCHED:",dbDetails)
 
+# get attendance details
+mycursor.execute("select * from attendance_record where u_id=%s",(u_id,))
+dbAttd = mycursor.fetchall()
+headers2=("Date","Attendance Detail")
+dbAttd.insert(0,headers2)
+print("ATTENDANCE OF CURRENT USER FETCHED:",dbAttd)
 
-# table 
+# table emp details
 for i in range(len(dbDetails)): #Rows
     for j in range(len(dbDetails[0])): #Columns
         b = tk.Entry(empDetailsFrame, width=20, fg='Black',font=('Arial',16,'bold'))
         b.insert(tk.END, dbDetails[i][j])
         b.grid(row=i, column=j)
 
-total_rows = len(dbDetails) 
-total_columns = len(dbDetails[0])
+empDetailsFrame.grid(row=1)
 
+# table emp attendance
+for i in range(len(dbAttd)): #Rows
+    for j in range(len(dbAttd[0])): #Columns
+        c = tk.Entry(attdFrame, width=33, fg='Black',font=('Arial',16,'bold'))
+        c.insert(tk.END, dbAttd[i][j])
+        c.grid(row=i, column=j)
 
-# Table1 = Table(empDetailsFrame)
-
-
+attdFrame.grid(row=3,pady=10)
 #table
 # Example(root).pack(fill="both", expand=True)
-empDetailsFrame.grid()
+
 
 # attendance button!
 
